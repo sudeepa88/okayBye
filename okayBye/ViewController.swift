81ac010a5a2497e108a217456d93ad10d1f1ae46
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-11-03 21:54:11 +0530

    Initial Commit

---
+++ okayBye/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  okayBye
+//
+//  Created by sudeepa pal on 03/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
